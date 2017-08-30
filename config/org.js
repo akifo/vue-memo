36c14d4d7f5ccc0f9510660d959ad4d193266c0c
akifo <akiho@bb07.net>
2017-08-30 20:32:46 +0900

    Create config folder

---
+++ config/org.js
@@ -0,0 +1,12 @@
+const FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}
+
+module.exports = {
+  FIREBASE_CONFIG
+}
