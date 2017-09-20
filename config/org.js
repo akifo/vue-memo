c62d6f7857598ad72da66c3da2e1907e343d5ef6
akifo <akiho@bb07.net>
2017-09-20 15:20:30 +0900

    Add configfiles

--- config/org.js
+++ config/org.js
@@ -1,3 +1,7 @@
+const YAML = require('yamljs')
+
+const RESERVED_WORDS = YAML.load('./config/reserved-words.yml')
+
 const FIREBASE_CONFIG = {
   apiKey: "",
   authDomain: "",
@@ -7,6 +11,16 @@ const FIREBASE_CONFIG = {
   messagingSenderId: ""
 }
 
+const AUTH_CONFIG = {
+  email: true,
+  google: false,
+  facebook: false,
+  twitter: false,
+  github: false
+}
+
 module.exports = {
-  FIREBASE_CONFIG
+  RESERVED_WORDS,
+  FIREBASE_CONFIG,
+  AUTH_CONFIG
 }
